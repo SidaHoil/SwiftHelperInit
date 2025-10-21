16f20b4de32202c3c3a4ac13e6df68a5cb5cbf06
SidaHoil <sidahoil88@gmail.com>
2025-10-21 19:43:03 +0700

    add load image form url on uiimageview

--- SwiftHelperInit/UIKit/UIImageView+Extension.swift
+++ SwiftHelperInit/UIKit/UIImageView+Extension.swift
@@ -13,4 +13,20 @@ extension UIImageView{
         self.init(image: UIImage(named: named))
         self.contentMode = contentMode
     }
+    
+    func loadImage(from: String, placeholder: UIImage? = nil) {
+        self.image = placeholder
+        guard let url = URL(string: from) else { return }
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let self = self,
+                  let data = data,
+                  let image = UIImage(data: data),
+                  error == nil
+            else { return }
+            
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }.resume()
+    }
 }
