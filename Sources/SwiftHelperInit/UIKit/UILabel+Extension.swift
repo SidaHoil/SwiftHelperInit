af688b46422e6e5977e2874c13517dbc4ea789b2
sida <35721499+SidaHoil@users.noreply.github.com>
2025-10-22 21:45:04 +0700

    Add convenience initializer for UILabel with numberOfLines

--- Sources/SwiftHelperInit/UIKit/UILabel+Extension.swift
+++ Sources/SwiftHelperInit/UIKit/UILabel+Extension.swift
@@ -13,12 +13,27 @@ public extension UILabel{
         self.init()
         self.text = text
     }
+
+    convenience init(text: String, numberOfLines: Int = 0) {
+        self.init()
+        self.text = text
+        self.numberOfLines = numberOfLines
+    }
     
     convenience init(text: String, textColor: UIColor = .label,font: UIFont = .systemFont(ofSize: 14), textAlignment: NSTextAlignment = .natural) {
         self.init()
         self.text = text
         self.textColor = textColor
         self.font = font
+        self.textAlignment = textAlignment
+    }
+
+    convenience init(text: String, textColor: UIColor = .label,font: UIFont = .systemFont(ofSize: 14), numberOfLines: Int = 0, textAlignment: NSTextAlignment = .natural) {
+        self.init()
+        self.text = text
+        self.textColor = textColor
+        self.font = font
+        self.numberOfLines = numberOfLines
         self.textAlignment = textAlignment
     }
 }
