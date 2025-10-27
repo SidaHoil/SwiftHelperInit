24c8b943b4d115201525886e71c9eacdb4868841
sida <35721499+SidaHoil@users.noreply.github.com>
2025-10-27 21:02:56 +0700

    Update UIEdgeInsets+Extension.swift

--- Sources/SwiftHelperInit/UIKit/UIEdgeInsets+Extension.swift
+++ Sources/SwiftHelperInit/UIKit/UIEdgeInsets+Extension.swift
@@ -9,15 +9,15 @@ import UIKit
 
 public extension UIEdgeInsets{
     
-    convenience init(vertical: CGFloat) {
+    init(vertical: CGFloat) {
         self.init(top: vertical, left: 0, bottom: vertical, right: 0)
     }
     
-    convenience init(horizontal: CGFloat) {
+    init(horizontal: CGFloat) {
         self.init(top: 0, left: horizontal, bottom: 0, right: horizontal)
     }
     
-    convenience init(all: CGFloat){
+    init(all: CGFloat){
         self.init(top: all, left: all, bottom: all, right: all)
     }
 }
